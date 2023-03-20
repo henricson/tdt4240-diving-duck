9b05ccf1309f0cc3f5b58901f6b8449460160cea
Herman Henriksen <hermanimittveien@gmail.com>
2023-03-20 12:50:23 +0100

    init

---
+++ core/src/com/divingduck/components/PositionComponent.java
@@ -0,0 +1,8 @@
+package com.divingduck.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+
+public class PositionComponent implements Component {
+    public Vector2 position = new Vector2();
+}
---
+++ ios/src/com/divingduck/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.divingduck.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.divingduck.game.DivingDuckGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new DivingDuckGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
