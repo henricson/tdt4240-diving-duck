aa26324a7767ef33aaffc3651ef7e9216594627f
Herman Henriksen <hermanimittveien@gmail.com>
2023-03-27 04:23:49 +0200

    Fixes

--- core/src/com/divingduck/components/PositionComponent.java
+++
@@ -1,8 +0,0 @@
-package com.divingduck.components;
-
-import com.badlogic.ashley.core.Component;
-import com.badlogic.gdx.math.Vector2;
-
-public class PositionComponent implements Component {
-    public Vector2 position = new Vector2();
-}
--- core/src/com/divingduck/components/VelocityComponent.java
+++
@@ -1,8 +0,0 @@
-package com.divingduck.components;
-
-import com.badlogic.ashley.core.Component;
-import com.badlogic.gdx.math.Vector2;
-
-public class VelocityComponent implements Component {
-    public Vector2 velocity = new Vector2();
-}
