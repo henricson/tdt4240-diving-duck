bfb6f3834708ee458e445ed5d1ec5d6c578e8a1e
kennbr <kennbr@stud.ntnu.no>
2023-04-14 18:12:45 +0200

    removed empty lines

--- core/src/com/divingduck/game/BirdSystem.kt
+++ core/src/com/divingduck/game/BirdSystem.kt
@@ -16,7 +16,7 @@ class BirdSystem(private val virtualHeight: Float) : EntitySystem() {
     private val positionMapper = ComponentMapper.getFor(PositionComponent::class.java)
     private val sizeMapper = ComponentMapper.getFor(SizeComponent::class.java)
     private val apiClient = ScoreApi("https://divingduckserver-v2.azurewebsites.net/")
-    private var shouldReportScore = true;
+    private var shouldReportScore = true
 
     override fun update(deltaTime: Float) {
         val birdEntities = engine.getEntitiesFor(birdFamily)
@@ -29,7 +29,6 @@ class BirdSystem(private val virtualHeight: Float) : EntitySystem() {
 
             if(positionComponent.position.y > 0) {
                 birdComponent.velocity.y +=  GRAVITY * deltaTime
-
             }
 
             if (birdComponent.isJumping) {
@@ -42,7 +41,7 @@ class BirdSystem(private val virtualHeight: Float) : EntitySystem() {
             // TODO: Når hastighet er integrert med posisjon burde vi ha inn at rotasjonen er beregnet av
             //  fuggelens hastighet i både x og y retning (altså ikke en 200F under)
 
-            rotationComponent.rotation = (-atan2(birdComponent.velocity.y.toFloat(), 200F) * 180 / Math.PI).toInt();
+            rotationComponent.rotation = (-atan2(birdComponent.velocity.y.toFloat(), 200F) * 180 / Math.PI).toInt()
 
             if (collisionMapper.has(birdEntity)) {
                 val collidable = collisionMapper.get(birdEntity)
@@ -62,7 +61,6 @@ class BirdSystem(private val virtualHeight: Float) : EntitySystem() {
                             println("Collision detected! Score is ${pipePosition.position.x}")
                             shouldReportScore = false
                         }
-
                     }
                 }
             }
